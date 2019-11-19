fdcad17cc832eb97f8f42a37ec98437b477502d8
linfeng <773639826@qq.com>
2019-11-19 11:14:02 +0800

    整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from 'vue-router'
+import VueRouter from 'vue-router'
+
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
